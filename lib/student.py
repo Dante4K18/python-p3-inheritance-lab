caae00d9a4153f8978374d3a980403c4114af0a1
Dante4K18 <dan@dan>
2024-10-03 04:42:02 +0300

    completed-inheritance-lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Initialize the User class
+        self.knowledge = []  # Initialize an empty knowledge list
+
+    def learn(self, knowledge_str):
+        self.knowledge.append(knowledge_str)  # Add knowledge to the list
